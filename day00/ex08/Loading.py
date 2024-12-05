276ec28ae870f7e0271c11c45479f9abce412133
Waso-python <waso.82@gmail.com>
2024-12-05 13:39:21 +0300

    fix:0day

--- day00/ex08/Loading.py
+++ day00/ex08/Loading.py
@@ -4,22 +4,22 @@ def ft_tqdm(lst: range) -> None:
     Does not use external imports.
     """
     total = len(lst)
-    
+
     for i, item in enumerate(lst, 1):
         # Calculate the percentage of completion
         percentage = (i / total) * 100
-        
+
         # Calculate the number of filled blocks of the progress bar
         bar_length = 42  # Length as in the original tqdm
         filled_length = int(bar_length * i / total)
-	
+
         # Create a progress bar with the █ symbol
         bar = '█' * filled_length + ' ' * (bar_length - filled_length)
-        
+
         # Form the progress string
         progress_str = f"\r{percentage:3.0f}%|{bar}| {i}/{total}"
-        
+
         # Print the progress
         print(progress_str, end='', flush=True)
-        
+
         yield item
--- day00/ex09/README.md
+++ day00/ex09/README.md
@@ -3,3 +3,35 @@
 A simple test package for demonstration purposes.
 
 ## Installation
+
+You can install the package using either of these commands:
+
+```bash
+pip install build
+```
+
+```bash	
+python -m build
+```
+
+```bash
+pip install ./dist/ft_package-0.0.1.tar.gz
+```
+or
+```bash
+pip install ./dist/ft_package-0.0.1-py3-none-any.whl
+```
+
+## Usage
+
+To use the package, you can import the functions as follows:
+```python
+from ft_package import count_in_list
+print(count_in_list(["toto", "tata", "toto"], "toto")) # output: 2
+print(count_in_list(["toto", "tata", "toto"], "tutu")) # output: 0
+```
+
+```bash
+pip list
+pip show -v ft_package
+```
